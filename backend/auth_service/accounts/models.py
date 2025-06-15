e1586e18b69673287a891014837498822ee6dc27
Erkaboy-coder <erkaboy756.5676@gmail.com>
2025-06-15 11:26:56 +0500

    new

---
+++ backend/auth_service/accounts/models.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    # Qo‘shimcha maydonlar kerak bo‘lsa shu yerga qo‘shing
+    pass
